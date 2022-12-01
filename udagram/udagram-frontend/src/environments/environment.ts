b94411243cbb5cb027762a835a14568b15a51f54
mosta1489 <mosta148999@gmail.com>
2022-12-01 21:31:07 +0200

    edit env

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:3000/api/v0',
+  appName: "Udagram",
+  apiHost:
+    "http://udagram-api-dev.eba-dhc2sg2t.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
